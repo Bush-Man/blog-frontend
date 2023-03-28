0246cb18eb10fba302442a53b5f4d079d6b1498c
Dennis Wachira <91716907+Bush-Man@users.noreply.github.com>
2023-03-29 01:16:39 +0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <Navbar />
       <div className="homeBody">
         <AdSection />
-    { err? <span style={{color:"red",fontSize:"20px",display:"flex",justifyContent:"center",textAlign:"center",width:"100%",height:"5rem"}}>The website is being updated...  </span> :  <>
+    { err? <span style={{color:"red",fontSize:"20px",display:"flex",flexDirection:"column",justifyContent:"center",textAlign:"center",width:"100%",height:"5rem"}}>The website is being updated... <img src="https://www.pinterest.com/pin/835206693388163415/" alt=""/> </span> :  <>
     
         <FeaturedPosts />
         <TopStories location="home" />
