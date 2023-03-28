7e270cc598d0b5e52422a8af31b254ce9b43e292
Dennis Wachira <91716907+Bush-Man@users.noreply.github.com>
2023-03-29 00:58:07 +0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,14 +6,18 @@ import TopStories from '../../TopStories/TopStories.jsx';
 import Footer from '../../Components/Footer/Footer.jsx';
 import Navbar from '../../Components/Navbar/Navbar';
 const Home = () => {
+  const err = true;
   return (
       <div className='home'>
       <Navbar />
       <div className="homeBody">
         <AdSection />
+    { err? <span style={{color:"red"}}>The site is being updated..  </span> :  <>
+    
         <FeaturedPosts />
         <TopStories location="home" />
-        
+       </>
+    }
       </div>
       <Footer />
     </div>
